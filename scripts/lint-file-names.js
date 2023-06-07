ce621626ee540ed69ec00f1d515585538bd4d1f1
Mathias Vagni <mathiasvagni@googlemail.com>
2023-06-07 10:57:46 +0200

    improve linting

---
+++ scripts/lint-file-names.js
@@ -0,0 +1,50 @@
+const { glob } = require('glob');
+
+// These files are exceptions
+const exceptions = [
+  'tsconfig.tsbuildinfo',
+  'tsconfig.json',
+  'theme.config.tsx',
+  'pnpm-lock.yaml',
+  'package.json',
+  'next.config.js',
+  'next-env.d.ts',
+  'README.md',
+];
+
+const codeFile = /[a-zA-Z]+(js|ts|tsx)/g;
+const pageRegex = /pages\/[a-z-\/]+.mdx/g;
+const pageMetaRegex = /pages\/[a-z-\/]*_meta.json/g;
+const imageRegex = /public\/[a-z-/0-9]+.(png|svg|ico)/g;
+
+async function lint() {
+  const errors = [];
+  const allFiles = await glob('**/**.*', { ignore: ['node_modules/**'] });
+
+  console.log(`👉 Linting ${allFiles.length} files.`);
+  allFiles.forEach((file) => {
+    const allowedPatterns = [pageRegex, pageMetaRegex, imageRegex, codeFile];
+
+    const isException = exceptions.includes(file);
+
+    if (isException) {
+      return;
+    }
+
+    const match = allowedPatterns.find((pattern) => {
+      return new RegExp(pattern).test(file);
+    });
+
+    if (!match) {
+      console.error(`❌ ${file}`);
+      errors.push(file);
+    }
+  });
+
+  if (errors.length) {
+    console.error(`\n${errors.length} files have invalid names.`);
+    process.exit(1);
+  }
+}
+
+lint();
